bc96658c261999e7bbe8dbfcb8e68a791e28bbc5
fbassa <fbassa98@gmail.com>
2025-02-21 16:03:18 +0000

    added new test
    subtract, divide, multiply, exponent, mod

--- test_math_utils.py
+++ test_math_utils.py
@@ -10,3 +10,44 @@ class TestMathUtils(unittest.TestCase):
         self.assertEqual(MathUtils.add(1.5, 2.5), 4.0)
         self.assertEqual(MathUtils.add(-1, 1), 0)
         self.assertEqual(MathUtils.add(-1.5, 1.5), 0.0)
+
+    def test_subtract(self):
+        """
+        """
+        self.assertEqual(MathUtils.subtract(2, 1), 1)
+        self.assertEqual(MathUtils.subtract(4, 2), 2.0)
+        self.assertEqual(MathUtils.subtract(1, 1), 0)
+        self.assertEqual(MathUtils.subtract(7, 3), 4.0)
+
+    def test_divide(self):
+        """
+        """
+        self.assertEqual(MathUtils.divide(2, 1), 2)
+        self.assertEqual(MathUtils.divide(4, 2), 2.0)
+        self.assertEqual(MathUtils.divide(10, 2), 5)
+        self.assertEqual(MathUtils.divide(9, 3), 3.0)
+
+    def test_multiply(self):
+        """
+        """
+        self.assertEqual(MathUtils.multiply(2, 1), 2)
+        self.assertEqual(MathUtils.multiply(4, 2), 8.0)
+        self.assertEqual(MathUtils.multiply(10, 2), 20)
+        self.assertEqual(MathUtils.multiply(9, 3), 27.0)
+
+    def test_exponent(self):
+        """
+        """
+        self.assertEqual(MathUtils.exponent(2, 1), 2)
+        self.assertEqual(MathUtils.exponent(4, 2), 16.0)
+        self.assertEqual(MathUtils.exponent(10, 2), 100)
+        self.assertEqual(MathUtils.exponent(9, 3), 729.0)
+
+    def test_mod(self):
+        """
+        """
+        self.assertEqual(MathUtils.mod(2, 1), 0)
+        self.assertEqual(MathUtils.mod(4, 2), 0)
+        self.assertEqual(MathUtils.mod(10, 3), 1)
+        self.assertEqual(MathUtils.mod(16, 3), 1.0)
+
