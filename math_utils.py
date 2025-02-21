cd3b99c8bf76694dde16aa189d89baa3e0f5ee86
fbassa <fbassa98@gmail.com>
2025-02-21 15:51:16 +0000

    added new metods
    muliply, exponent, mod

--- math_utils.py
+++ math_utils.py
@@ -2,12 +2,10 @@ class MathUtils:
 
   @staticmethod
   def add(a: int | float, b: int | float) -> int | float:
-
     return a + b
 
   @staticmethod
   def subtract(a: int | float, b: int | float) -> int | float:
-
     return a - b
 
   @staticmethod
@@ -17,6 +15,28 @@ class MathUtils:
       raise ValueError("You cannot divide by zero!")
 
     return a / b
+
+  @staticmethod
+  def multiply(a: int | float, b: int | float) -> float:
+
+    return a * b
+
+  @staticmethod
+  def exponent(a: int | float, b: int | float) -> float:
+    return a ** b
+
+  @staticmethod
+  def mod(a: int | float, b: int | float) -> float:
+    if b == 0:
+      raise ValueError("You cannot divide by zero!")
+    return a % b
+
+
+
+
+
+
+
 
 
 
